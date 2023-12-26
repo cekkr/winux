f227005179cc26a8127d8d8aa24f1bf259cd16db
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-12-26 13:18:27 -0800

    Update host.js

--- host.js
+++ host.js
@@ -4,23 +4,49 @@ const vmName = "Arch Linux"; // Replace with your VM's name
 
 // Function to execute a command in the VM
 function vmExec(command) {      
-    return new Promise((res, err)=>{        
-        const vboxCommand = `VBoxManage guestcontrol "Arch Linux" run --exe "/bin/bash" --username "root" --password "" --wait-stderr --wait-stdout -- -c "${command}"`;
+    return new Promise((res, err)=>{   
+        setTimeout(()=>{
+            console.log("Executing: " + command)
+            const vboxCommand = `VBoxManage guestcontrol "Arch Linux" run --exe "/bin/bash" --username "root" --password "" --wait-stderr --wait-stdout -- -c "${command}"`;
 
-        exec(vboxCommand, (error, stdout, stderr) => {
-            if (error) {
-                err({error, stderr});
-                return;
-            }
-            res({stdout, stderr});
-        });
+            exec(vboxCommand, (error, stdout, stderr) => {
+
+                if(stderr) console.error(stderr)
+                if(stdout) console.log(stdout)
+
+                if ((!stdout && !stderr) && error) {
+                    throw error
+                }
+
+                res({stdout, stderr});
+            });
+        }, 100);
     })
 }
 
 // Example usage
 async function main(){
-    let res = await vmExec("ls /")
-    console.log("res: ", res)
+    let res = await vmExec("cat /sys/firmware/efi/fw_platform_size")
+    let hasEfi = res.stdout.startsWith('64')
+
+    if(hasEfi)
+        console.log('Has EFI!')
+    else 
+        console.log('No EFI...')
+
+    // Connect to internet
+    await vmExec("iplink")
+
+    // Update time 
+    await vmExec("timedatectl")
+
+    // Partitions
+    let rFDisk = await vmExec("fdisk -l")
+
+    return
+    // Install node
+    res = await vmExec("sudo pacman -Syu --noconfirm nodejs")
+    console.log("Node install res: ", res)
 }
 
 main()
