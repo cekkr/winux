52840e3fda714b22a31526b6343e52540be3b08c
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-12-27 16:49:20 -0800

    ssh

--- ssh.js
+++ ssh.js
@@ -10,18 +10,24 @@ const sshConfig = {
 };
 
 conn.on('ready', () => {
-  console.log('Client :: ready');
-  conn.shell((err, stream) => {
-    if (err) throw err;
-    
-    stream.on('close', () => {
-      console.log('Stream :: close');
-      conn.end();
-    }).on('data', (data) => {
-      console.log('OUTPUT: ' + data);
-    });
+    console.log('Client :: ready');
+    conn.shell((err, stream) => {
+        if (err) throw err;
+        
+        stream.on('close', () => {
+            console.log('Stream :: close');
+            conn.end();
+        }).on('data', (data) => {
+            console.log('OUTPUT: ' + data.toString());
+        });
 
-    // Execute commands in sequence
-    stream.end('ls -l\nexit\n'); // Add your commands here
-  });
+        // Send a command without ending the stream
+        stream.write('ls /\n');
+
+        // You can send more commands later by calling stream.write again
+        // For example, after some time or based on some conditions:
+        setTimeout(() => {
+            stream.write('echo "Another command"\n');
+        }, 5000); // Execute another command after 5 seconds
+    });
 }).connect(sshConfig);
