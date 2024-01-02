ea45f3396b162b716c2ecc8ea774c34a155ffa77
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-01-02 15:55:32 -0800

    new bassh

--- 3.ssh_install.js
+++ 3.ssh_install.js
@@ -1,8 +1,35 @@
-import {ssh} from './libs/ssh.js'
+import * as config from './config.js'
+import {Bassh} from 'bassh'
 
-ssh.onReady = async ()=>{
-    let res = await ssh.cmd('echo ciao')
-    console.log('req res ', res)
-}
+const bassh = Bassh({
+    host: config.IP,
+    port: 22,
+    username: 'user',
+    password: 'pass' // or use privateKey for key-based authentication
+})
 
-ssh.start()
+bassh.onReady = async function(){
+    console.log("bash ready")
+ 
+    let sudoRes = await bassh.sudoSu('pass')
+    console.log("Sudo su: ", sudoRes)
+
+    if(sudoRes.wrongPassword){
+        console.error("Wrong sudo password.");
+        return;
+    }
+ 
+    // Real time output
+    let pres = bassh.cmd('echo meow')
+    
+    pres.out = async (out)=>{
+    console.log("Real time stdout: ", out)
+    }
+        
+    pres.err = async (out)=>{
+    console.log("Real time stderr: ", out)
+    }
+    
+    pres = await pres;
+    console.log("bash res: ", pres)
+ }
