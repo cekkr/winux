1883c2f0b97332634c2725ec06508af9f8a6f1e8
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-01-03 05:46:09 -0800

    check if host is alive

--- 3.ssh_install.js
+++ 3.ssh_install.js
@@ -1,35 +1,50 @@
 import * as config from './config.js'
 import {Bassh} from 'bassh'
 
-const bassh = Bassh({
-    host: config.IP,
-    port: 22,
-    username: 'user',
-    password: 'pass' // or use privateKey for key-based authentication
-})
+import * as ping from 'ping'
+import * as installedBoot from './libs/installedBoot.js'
 
-bassh.onReady = async function(){
-    console.log("bash ready")
- 
-    let sudoRes = await bassh.sudoSu('pass')
-    console.log("Sudo su: ", sudoRes)
+const host = 'example.com'; // Replace with the hostname or IP address of the server you want to ping.
 
-    if(sudoRes.wrongPassword){
-        console.error("Wrong sudo password.");
-        return;
+ping.sys.probe(config.IP, async (isAlive) => {
+    if(!isAlive){
+        await installedBoot.connectToVMNetwork()
     }
- 
-    // Real time output
-    let pres = bassh.cmd('echo meow')
+
+    startBassh()
+});
+
+function startBassh(){
+    const bassh = Bassh({
+        host: config.IP,
+        port: 22,
+        username: 'user',
+        password: 'pass' // or use privateKey for key-based authentication
+    })
+
+    bassh.onReady = async function(){
+        console.log("bash ready")
     
-    pres.out = async (out)=>{
-    console.log("Real time stdout: ", out)
-    }
+        let sudoRes = await bassh.sudoSu('pass')
+        console.log("Sudo su: ", sudoRes)
+
+        if(sudoRes.wrongPassword){
+            console.error("Wrong sudo password.");
+            return;
+        }
+    
+        // Real time output
+        let pres = bassh.cmd('echo meow')
         
-    pres.err = async (out)=>{
-    console.log("Real time stderr: ", out)
+        pres.out = async (out)=>{
+        console.log("Real time stdout: ", out)
+        }
+            
+        pres.err = async (out)=>{
+        console.log("Real time stderr: ", out)
+        }
+        
+        pres = await pres;
+        console.log("bash res: ", pres)
     }
-    
-    pres = await pres;
-    console.log("bash res: ", pres)
- }
+}
